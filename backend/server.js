657c950fdc0fe257297d66a0556ab4ce80fe36d2
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-06-29 00:01:10 +0530

    f login

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,26 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.listen(8000);
-
-// middleware
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", (req, res, next) => {
-  console.log(req.method, req.url);
-  next();
+app.post("/", (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    if (username === "admin" && password === "password") {
+      console.log(username, password);
+      res.status(200).json({ message: "success" });
+    } else {
+      res.status(400).json({ error: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.get("/", (req, res) => {
-  res.json({ msg: "succes" });
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,3 @@
-// src/Login.js
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
@@ -9,13 +8,44 @@ const Login = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
 
-  const handleLogin = () => {
-    // Perform authentication
-    if (username === "admin" && password === "password") {
+  // const handleLogin = () => {
+  //   // Perform authentication
+  //   if (username === "admin" && password === "password") {
+  // login();
+  // navigate("/dashboard");
+  //   } else {
+  //     alert("Invalid credentials");
+  //   }
+  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const loginCredentials = { username, password };
+
+    try {
+      const response = await fetch("http://localhost:8000/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginCredentials),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      console.log("Response:", json);
+
+      setUsername("");
+      setPassword("");
+      console.log("Login success");
       login();
       navigate("/dashboard");
-    } else {
-      alert("Invalid credentials");
+    } catch (error) {
+      console.error("Error during login:", error);
     }
   };
 
@@ -34,7 +64,7 @@ const Login = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleSubmit}>Login</button>
     </div>
   );
 };
