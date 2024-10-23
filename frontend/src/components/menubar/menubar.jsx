8ff068810506265faf3964f45ddacb6d962a8909
ayeshan weerasuriya <ayeshanweerasuriya@gmail.com>
2024-10-23 21:02:41 +0530

    f-imp-routes/menubar

---
+++ frontend/src/components/menubar/menubar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { Avatar, Divider } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import Button from "../button/Button";
+import "./menubar.css";
+const MenuBar = () => {
+  return (
+    <div className="menu-bar">
+      <Avatar size={"large"} icon={<UserOutlined />} />
+      <br />
+      <span>{"melani@gmail.com"}</span>
+      <Divider />
+      <Link to="/appointments">
+        <Button text={"Appointments"} />
+      </Link>
+      <Link to="/view-records">
+        <Button text={"View Records"} />
+      </Link>
+      <Link to="/add-patients">
+        <Button text={"Add Patients"} />
+      </Link>
+      <Link to="/update-patients">
+        <Button text={"Update Patients"} />
+      </Link>
+      <Link to="/delete-patients">
+        <Button text={"Delete Patients"} />
+      </Link>
+      <Divider />
+      <Button text={"LogOut"} />
+    </div>
+  );
+};
+
+export default MenuBar;
---
+++ frontend/src/views/DeletePatients.jsx
@@ -0,0 +1,7 @@
+export function DeletePatients() {
+  return (
+    <div>
+      <h1>Delete Patients</h1>
+    </div>
+  );
+}
