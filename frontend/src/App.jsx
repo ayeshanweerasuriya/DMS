acfde741ccd46df30d5e9bde42018c6a1005491c
Melanie Sankalpana <uamsankalpana@gmail.com>
2024-10-28 23:16:45 +0530

    app.jsx commit test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { MenuBar } from "./components/menubar/MenuBar";
-import { DashComponent } from "./components/dash-component/DashComponent";
+import { MenuBar } from "./components/menubar/ManuBar";
 import { Appointments } from "./views/appointments";
 import { ViewRecords } from "./views/ViewRecords";
 import { AddPatients } from "./views/AddPatients";
@@ -10,31 +9,22 @@ import { LogIn } from "./views/login/LogIn";
 
 function App() {
   return (
-    // <div>
-    // <LogIn />
-    // </div>
-    <Router>
-      <div style={{ display: "flex", minHeight: "100vh" }}>
-        <div style={{ width: "300px" }}>
-          <MenuBar />
-
-          <DashComponent>
-            <p>Additional Details</p>
-          </DashComponent>
-        </div>
-
-        <div style={{ flex: 1, padding: "20px" }}>
-          <Routes>
-            <Route path="/appointments" element={<Appointments />} />
-            <Route path="/view-records" element={<ViewRecords />} />
-            <Route path="/add-patients" element={<AddPatients />} />
-            <Route path="/update-patients" element={<UpdatePatients />} />
-            <Route path="/delete-patients" element={<DeletePatients />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-
+    <div>
+      <LogIn />
+    </div>
+    // <Router>
+    //   <div style={{ display: "flex", minHeight: "100vh" }}>
+    //     <div style={{ width: "300px" }}>
+    //       <MenuBar />
+    //     </div>
+    //     <div style={{ flex: 1, padding: "20px" }}>
+    //       <Routes>
+    //         <Route path="/appointments" element={<Appointments />} />
+    //         <Route path="/view-records" element={<ViewRecords />} />
+    //         <Route path="/add-patients" element={<AddPatients />} />
+    //         <Route path="/update-patients" element={<UpdatePatients />} />
+    //         <Route path="/delete-patients" element={<DeletePatients />} />
+    //       </Routes>
     //     </div>
     //   </div>
     // </Router>
