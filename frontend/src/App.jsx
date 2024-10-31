5317da451c4b5c3468c6b78acbd90488368e6a4f
ayeshan weerasuriya <ayeshanweerasuriya@gmail.com>
2024-10-31 13:53:03 +0530

    Refactor App component to use Ant Design layout and routing; update CSS for improved styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,99 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { MenuBar } from "./components/menubar/ManuBar";
-import { Appointments } from "./views/appointments";
-import { ViewRecords } from "./views/ViewRecords";
-import { AddPatients } from "./views/AddPatients";
-import { UpdatePatients } from "./views/UpdatePatients";
-import { DeletePatients } from "./views/DeletePatients";
-// import { LogIn } from "./views/login/LogIn";
+import { Routes, Route, Link } from "react-router-dom";
+import { Button, Layout, Menu, theme } from "antd";
+import { useState } from "react";
+import {
+  UsergroupAddOutlined,
+  FolderViewOutlined,
+  UpCircleOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  DeleteOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+import { Appointments } from "./views/appointments/Appointments";
+import { ViewRecords } from "./views/view-patients/ViewRecords";
+import { AddPatients } from "./views/add-patients/AddPatients";
+import { UpdatePatients } from "./views/update-patients/UpdatePatients";
+import { DeletePatients } from "./views/delete-patients/DeletePatients";
 
-function App() {
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const { Header, Sider, Content } = Layout;
+
+  const borderRadiusLG = "25px";
+  const colorBgContainer = theme === "dark" ? "#b0ccfc" : "#fff";
+
+  const menuItems = [
+    {
+      key: "1",
+      icon: <UsergroupAddOutlined />,
+      label: <Link to="/appointments">Appointments</Link>,
+    },
+    {
+      key: "2",
+      icon: <FolderViewOutlined />,
+      label: <Link to="/view-records">View Records</Link>,
+    },
+    {
+      key: "3",
+      icon: <UserAddOutlined />,
+      label: <Link to="/add-patients">Add Patients</Link>,
+    },
+    {
+      key: "4",
+      icon: <UpCircleOutlined />,
+      label: <Link to="/update-patients">Update Patients</Link>,
+    },
+    {
+      key: "5",
+      icon: <DeleteOutlined />,
+      label: <Link to="/delete-patients">Delete Patients</Link>,
+    },
+  ];
+
   return (
-    // <div>
-    // <LogIn />
-    // </div>
-    <Router>
-      <div style={{ display: "flex", minHeight: "100vh" }}>
-        <div style={{ width: "300px" }}>
-          <MenuBar />
-        </div>
-        <div style={{ flex: 1, padding: "20px" }}>
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        style={{ backgroundColor: "#fff" }}
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="light"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={menuItems}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
           <Routes>
             <Route path="/appointments" element={<Appointments />} />
             <Route path="/view-records" element={<ViewRecords />} />
@@ -25,10 +101,10 @@ function App() {
             <Route path="/update-patients" element={<UpdatePatients />} />
             <Route path="/delete-patients" element={<DeletePatients />} />
           </Routes>
-        </div>
-      </div>
-    </Router>
+        </Content>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
