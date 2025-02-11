a1a5bf84216379251f73cd41838840df0b77ea4b
Melanie Sankalpana <uamsankalpana@gmail.com>
2025-02-12 01:43:35 +0530

    Create and Get Appointments FE

--- backend/models/dental/Appointment.js
+++ backend/models/dental/Appointment.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 const appointmentSchema = new mongoose.Schema({
    
     patientName: { type: String, required: true }, // Patient's Name
-    appointmentDate: { type: Date, required: true }, // Appointment Date
+    appointmentDate: {
+        type: Date, 
+        required: true,
+        validate: {
+            validator: function(value) {
+                return value >= new Date().setHours(0, 0, 0, 0); // Allow only today or future dates
+            },
+            message: "Appointment date should be today or future"
+        }
+    },
+    
     appointmentTime: { type: String, required: true }, // Appointment Time
-    contactNumber: { type: String, required: true }, // Contact Number
-    patientAge: { type: Number, required: true }, // Patient Age
+    contactNumber: { type: String, required: true,match: /^[0-9]{1,15}$/ }, // Contact Number
+    patientAge: { type: Number, required: true, min: 0, max: 150 }, // Patient Age
     createdAt: { type: Date, default: Date.now } // Timestamp
 });
 
